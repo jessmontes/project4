3f6020d974dd73a52424da6a6cf48879b8083d0e
Jessica Montes <jmonte29@stu.southtexascollege.edu>
2024-09-20 02:25:51 -0500

    added the recipe item, cost, calories, units

---
+++ src/components/RecipeItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h3>{{ recipe.name }}</h3>
+      <ul>
+        <li v-for="(ingredient, index) in recipe.ingredients" :key="index">
+          {{ ingredient.name }}: {{ ingredient.quantity }} units, ${{ ingredient.cost }}, {{ ingredient.calories }} calories
+        </li>
+      </ul>
+      <p>Total Cost: ${{ totalCost }}</p>
+      <p>Total Calories: {{ totalCalories }} kcal</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['recipe'],
+    computed: {
+      totalCost() {
+        return this.recipe.ingredients.reduce((sum, ingredient) => sum + ingredient.cost, 0);
+      },
+      totalCalories() {
+        return this.recipe.ingredients.reduce((sum, ingredient) => sum + ingredient.calories, 0);
+      }
+    }
+  };
+  </script>
+  
