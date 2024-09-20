c7a6ababd8d1cabfb951042da4994c73c6b411b8
Jessica Montes <jmonte29@stu.southtexascollege.edu>
2024-09-20 02:24:30 -0500

    created recipe list

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h2>Recipe List</h2>
+      <div v-for="(recipe, index) in recipes" :key="index">
+        <RecipeItem :recipe="recipe" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import RecipeItem from './RecipeItem.vue';
+  
+  export default {
+    props: ['recipes'],
+    components: {
+      RecipeItem
+    }
+  };
+  </script>
+  
