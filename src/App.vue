dba4d1614f01a51021cc6a2e3b32b32f88fceb2f
Jessica Montes <jmonte29@stu.southtexascollege.edu>
2024-09-20 02:27:41 -0500

    .

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Recipe Cost and Calorie Tracker</h1>
+    <RecipeForm @add-recipe="addRecipe" />
+    <RecipeList :recipes="recipes" />
+  </div>
+</template>
+
+<script>
+import RecipeForm from './components/RecipeForm.vue';
+import RecipeList from './components/RecipeList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RecipeForm,
+    RecipeList
+  },
+  data() {
+    return {
+      recipes: []
+    };
+  },
+  methods: {
+    addRecipe(recipe) {
+      this.recipes.push(recipe);
+    }
+  }
+};
+</script>
